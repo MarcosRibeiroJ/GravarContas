a15e792d4a43724f252a5d74f17940ae4c7ac609
Marcos Ribeiro <marcossrj90@gmail.com>
2017-03-19 22:14:44 -0300

    Criado metodo gravar, adicionado uso do metodo gravar nos metodos cadastrar e sacarSistema

--- br/com/desafiocontas/OperacaoSistema.java
+++ br/com/desafiocontas/OperacaoSistema.java
@@ -16,8 +16,9 @@ public class OperacaoSistema {
 	
 	//A lista de contas sera usada por outros metodos, portando deve ser da classe
 	ArrayList<Conta2> contas = null;
-	String path = "C:/Users/Markinho/cadastro2.txt";
+	String path = "C:/Users/Markinho/cadastro2.txt", escolha;
 	File arquivo;
+	int indice = 0;
 
 	//criado metodo que cadastra conta com nome e saldo de usuario
 	public void cadastrar() {
@@ -25,65 +26,31 @@ public class OperacaoSistema {
 		//string para nome do cliente, op para parar o laco e path com caminho do txt criado
 		String codConta, nome, op;
 		
-		//inteiro para percorrer o array
-		int indice = 0;
-		
 		//array que ira armazenar as contas
 		this.contas = new ArrayList<Conta2>();
-		
-		//criado objeto do tipo arquivo
-		this.arquivo = new File(this.path);
-		
-		//criado objeto que tem a funcao de escrever informacoes no buffer de memoria
-		//esse objeto recebe como parametro um outro objeto do tipo FileWriter para escrever em arquivo e true para continuar do ponto em q o texto parou
-		//esse objeto eh criado dentro de uma estrutura try, catch para tratar possiveis erros de I/O 
-		//no caso desses objetos eh obrigatoria a criacao de uma estrutura dessas pq um arquivo a gravacao em arquivo pode apresentar varios erros, desde permissao de gravacao, arquivo corrompido, etc
-		try(BufferedWriter escrever = new BufferedWriter(new FileWriter(arquivo, true))) {
-			Scanner leitor = new Scanner(System.in);//scanner para ler dados
-			do {
-				System.out.println("Digite o codigo da conta: ");
-				codConta = leitor.nextLine();
-				System.out.println("Digite o nome do cliente: ");
-				nome = leitor.nextLine();
-				System.out.println("Digite o saldo do cliente: ");
-				//criacao de objeto do tipo conta que recebe os dados do saldo e ja cria um objeto do tipo pessoa para armazenar o nome
-				Conta2 conta = new Conta2(codConta, new Cliente(nome), leitor.nextDouble());
-				contas.add(conta);//adicionando objeto conta no arraylist
 				
-				if(arquivo.length()>0) {//se o arquivo ja tiver conteudo ...
-					escrever.newLine();//pule linha
-					//adicione os dados de nome e saldo no txt
-					escrever.write("Conta: " + contas.get(indice).getCodConta() + " Cliente: " + contas.get(indice).getCliente().getNome() + " Saldo: R$ " + contas.get(indice).getSaldo());
-					//gravar conteudo
-					escrever.flush();
-				} else {
-					escrever.write("Conta: " + contas.get(indice).getCodConta() + " Cliente: " + contas.get(indice).getCliente().getNome() + " Saldo: R$ " + contas.get(indice).getSaldo());
-					escrever.flush();
-				}
-				//controle para sair do laco ou continuar cadastrando contas
-				System.out.println("Deseja cadastrar outra conta? ");
-				op = leitor.nextLine();
-				indice++;
-			} while(!"nao".equals(op = leitor.nextLine()));
-		} catch(IOException e) {//capturando erro caso aconteca e imprimindo o caminho do erro
-			e.printStackTrace();
-		}
+		Scanner leitor = new Scanner(System.in);//scanner para ler dados
+		do {
+			System.out.println("Digite o codigo da conta: ");
+			codConta = leitor.nextLine();
+			System.out.println("Digite o nome do cliente: ");
+			nome = leitor.nextLine();
+			System.out.println("Digite o saldo do cliente: ");
+			//criacao de objeto do tipo conta que recebe os dados do saldo e ja cria um objeto do tipo pessoa para armazenar o nome
+			Conta2 conta = new Conta2(codConta, new Cliente(nome), leitor.nextDouble());
+			contas.add(conta);//adicionando objeto conta no arraylist
+			gravar();//chamada do metodo que grava dados no arquivo
+			//controle para sair do laco ou continuar cadastrando contas
+			System.out.println("Deseja cadastrar outra conta? ");
+			op = leitor.nextLine();
+			indice++;
+		} while(!"nao".equals(op = leitor.nextLine()));
 	}//fim cadastrar
-	
 	
 	public void sacarSistema() {
 		
-		/**
-		 * ainda falta:
-		 * gravar a alteração no txt
-		 * criar um laco depois que terminar o saque para voltar para o metodo menu que nao criei ainda
-		 */
-		
 		//valor do saque
 		double valorSaque;
-		
-		//ira armazenar a localizacao da conta no array, seu indice
-		int numConta;
 		
 		//codigo da conta que sera pesquisado
 		String codConta;
@@ -97,17 +64,16 @@ public class OperacaoSistema {
 			System.out.println("Digite qualquer tecla para cadastrar contas ou nao para sair:");
 			
 			//caso o usuario queira cadastrar conta
-			//por enquanto estou criando as contas, no futuro vai chamar o metodo cadastrar
 			if(!"nao".equals(leitor.nextLine())) {
-				cadastrar();
+				cadastrar();//chamada do metodo que cadastra contas
 			}
 		}
 		do {
 			//depois de ter as contas criadas, digitar o codigo de alguma para sacar
 			System.out.println("Digite o codigo da conta para saque: ");
 			
-			//armazena o valor digitado
-			codConta = leitor.nextLine();
+			//armazena o valor digitado, usei o next para corrigir um conflito, se usar nextLine vai dar conflito nos casos em que nao localiza a conta cadastrada
+			codConta = leitor.next();
 			
 			//chamo o metodo pesquisar passando o array de contas e o codigo
 			//se nao encontrar ele retorna -1, se encontrar retorna a posicao da conta no array
@@ -117,22 +83,23 @@ public class OperacaoSistema {
 				System.out.println("Digite o valor do saque: ");
 				valorSaque = leitor.nextDouble();
 				
-				//numConta recebe o inteiro com a posicao da conta no array
-				numConta = pesquisar(contas, codConta);
+				//indice recebe o inteiro com a posicao da conta no array
+				indice = pesquisar(contas, codConta);
 				
-				//crio uma opercao de conta para sacar
+				//crio uma operacao de conta para sacar
 				OperacaoConta s = new OperacaoConta();
 				
 				//passo como parametro para o metodo a conta e o valor a sacar
-				s.sacar(contas.get(numConta), valorSaque);
-				System.out.println("Cliente: " + contas.get(numConta).getCliente().getNome() + " saldo: R$ " + contas.get(numConta).getSaldo());
+				s.sacar(contas.get(indice), valorSaque);
+				System.out.println("Cliente: " + contas.get(indice).getCliente().getNome() + " saldo: R$ " + contas.get(indice).getSaldo());
+				gravar();//chamada do metodo que grava o resultado do saque no txt
 			}
+			//controle para parar de sacar quando o usuario digitar sair
 			System.out.println("Digite qualquer tecla para realizar outro saque ou sair para encerrar: ");
 			leitor.nextLine();
-		} while(!"sair".equals(leitor.nextLine()));
+			escolha = leitor.next();//usei next para corrigir conflito que aparece quando uso nextLine
+		} while(!"sair".equals(escolha));
 	}//fim sacarSistema
-	
-	
 	
 	public int pesquisar(ArrayList<Conta2> contas, String codConta) {
 		
@@ -168,5 +135,30 @@ public class OperacaoSistema {
 			return indiceConta;
 		}
 	}//fim pesquisar
-
+	
+	public void gravar() {
+		
+		//criado objeto do tipo arquivo
+		this.arquivo = new File(this.path);
+		
+		//criado objeto que tem a funcao de escrever informacoes no buffer de memoria
+		//esse objeto recebe como parametro um outro objeto do tipo FileWriter para escrever em arquivo e true para continuar do ponto em q o texto parou
+		//esse objeto eh criado dentro de uma estrutura try, catch para tratar possiveis erros de I/O 
+		//no caso desses objetos eh obrigatoria a criacao de uma estrutura dessas pq um arquivo a gravacao em arquivo pode apresentar varios erros, desde permissao de gravacao, arquivo corrompido, etc
+		try(BufferedWriter escrever = new BufferedWriter(new FileWriter(arquivo, true))) {
+			if(arquivo.length()>0) {//se o arquivo ja tiver conteudo ...
+				escrever.newLine();//pule linha
+				//adicione os dados de nome e saldo no txt
+				escrever.write("Conta: " + contas.get(indice).getCodConta() + " Cliente: " + contas.get(indice).getCliente().getNome() + " Saldo: R$ " + contas.get(indice).getSaldo());
+				//gravar conteudo
+				escrever.flush();
+			} else {
+				escrever.write("Conta: " + contas.get(indice).getCodConta() + " Cliente: " + contas.get(indice).getCliente().getNome() + " Saldo: R$ " + contas.get(indice).getSaldo());
+				escrever.flush();
+			}
+		} catch(IOException e) {//capturando erro caso aconteca e imprimindo o caminho do erro
+			e.printStackTrace();
+		}
+	}//fim metodo gravar
+	
 }//fim classe
